3c871a4cdd31acd33fea7f557810fc9af4f7fa14
Akdev Saha <akdevsaha2003@gmail.com>
2025-02-01 04:56:16 +0530

    chore:userSchema is added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js"; // ESM import path
 
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 const app = express();
+dotenv.config();
 
+const PORT = process.env.PORT;
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => console.log("Server is running on port number 3000"));
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+  connectDB();
+});
